5d7ef27478306e78a603540e5dd68142fd00bcc5
Taite Jernigan <taite5100@gmail.com>
2022-05-11 13:08:30 -0700

    Replaced render with createRoot

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App/>);
